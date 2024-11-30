c1c9a8d3362027482d7b5ea0b666bc2151c0d277
Martin <xarpentz@gmail.com>
2024-11-30 15:41:54 +0500

    finalfunc

--- news/models.py
+++ news/models.py
@@ -10,6 +10,9 @@ class Articles(models.Model):
     def __str__(self):
         return f'News: {self.title}'
 
+    def get_absolute_url(self):
+        return f'/news/{self.id}'
+
     class Meta:
         verbose_name = 'News'
         verbose_name_plural = 'Newses'
